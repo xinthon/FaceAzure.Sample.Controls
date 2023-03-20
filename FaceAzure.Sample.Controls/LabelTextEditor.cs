619a1040c60b951041cf32fceb67fea1f1c861f7
Sinthon <s.sinthon@cas.com.kh>
2023-03-20 14:18:40 +0700

    Add project files.

---
+++ FaceAzure.Sample.Controls/LabelTextEditor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FaceAzure.Sample.Controls
+{
+    public class LabelTextEditor : Control
+    {
+        public static DependencyProperty LabelStateProperty = DependencyProperty.Register("LabelState", typeof(State), typeof(LabelTextEditor), new PropertyMetadata(State.Normal));
+        public static DependencyProperty ContentProperty = DependencyProperty.Register("Content", typeof(string), typeof(LabelTextEditor), new PropertyMetadata(string.Empty));
+
+        public State LabelState
+        {
+            get => (State)GetValue(LabelStateProperty);
+            set => SetValue(LabelStateProperty, value);
+        }
+
+        public string Content
+        {
+            get => (string)GetValue(ContentProperty);
+            set => SetValue(ContentProperty, value);
+        }
+
+        static LabelTextEditor()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LabelTextEditor), new FrameworkPropertyMetadata(typeof(LabelTextEditor)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+        }
+
+    }
+    public enum State
+    {
+        Normal,
+        Edit
+    }
+}
---
+++ FaceAzure.Sample.Controls/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FaceAzure.Sample.Controls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window , INotifyPropertyChanged
+    {
+        private string sampleString =  string.Empty;
+
+        public string SampleString
+        { 
+            get => sampleString; 
+            set {
+                sampleString = value;
+                OnPropertyChanged(nameof(SampleString));
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext= this;
+
+            this.btnOne.Click += delegate
+            {
+                if(labelOne.LabelState == State.Edit)
+                {
+                    labelOne.LabelState = State.Normal;
+                    return;
+                }
+
+                labelOne.LabelState = State.Edit;
+            };
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
